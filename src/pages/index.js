e3f1f279af61a4dc072d52de5b1c7dcc112c5b93
Atiti1 <atitidestiny17@gmail.com>
2021-10-16 16:24:48 +0100

    added new component to tthe project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,21 @@
 import * as React from "react"
 import { graphql, Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { node } from "prop-types"
+
+
+const BlogLink = styled(Link)`
+   text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
 
 export default ({ data }) => {
   console.log(data)
@@ -13,12 +24,14 @@ export default ({ data }) => {
     <SEO  title="Home" />
     <div>
       <h1>Atiti's Thoughts</h1>
-      <h4>{data.allMarkdownRemark.totalCount}</h4>
+      <h4>{data.allMarkdownRemark.totalCount} Posts </h4>
       {data.allMarkdownRemark.edges.map(({ node }) => (
       <div key={node.id}>
-        <span>
+        <BlogLink to={node.fields.slug}>
+        <BlogTitle>
           {node.frontmatter.title} - {node.frontmatter.date}
-          </span>
+          </BlogTitle>
+          </BlogLink>
           <p>{node.excerpt}</p>
           </div>
       ))}
@@ -29,7 +42,7 @@ export default ({ data }) => {
 
 export const query = graphql`
 query {
-  allMarkdownRemark {
+  allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
     totalCount
     edges {
       node {
@@ -38,6 +51,9 @@ query {
           title
           description
           date
+        }
+        fields {
+          slug
         }
         excerpt
       }
